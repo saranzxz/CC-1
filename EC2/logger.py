234f4aeef5260e5364ea7123fc0268b3fb648d0b
saranzxz <sprasa33@asu.edu>
2023-09-21 19:01:43 -0700

    logger fix

--- EC2/logger.py
+++ EC2/logger.py
@@ -1,13 +1,22 @@
-import logging
+# import logging
 
-logging.basicConfig(
-    filename = './EC2/logs.txt',
-    filemode = 'a',
-    #format = '%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-    datefmt = '%H:%M:%S',
-    level = logging.DEBUG
-)
+# logging.basicConfig(
+#     filename = './EC2/logs.txt',
+#     filemode = 'a',
+#     #format = '%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+#     datefmt = '%H:%M:%S',
+#     level = logging.DEBUG
+# )
+
+# def log(level, message):
+#     print(level, message)
+#     logging.debug(message)
+
+from datetime import datetime
+
+file = './EC2/logs.txt'
 
 def log(level, message):
-    print(level, message)
-    logging.debug(message)
+    fh = open(file, 'a')
+    fh.write(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + '\t' + level + '\t' + message + '\n')
+    fh,close()
